334b29a8bc9da4e58e315996a0987ec352e6c69f
MaxCLZ <maxence.clusaz@gmail.com>
2023-01-11 14:45:43 +0100

    desktop responsive

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 let cv = document.getElementById('cv');
 
 const projectList = [...document.getElementsByClassName('project')]
-
+console.log(projectList);
 
 const openOrCloseDetails = (event) =>{
-    event.target.lastElementChild.style.display === '' ? event.target.lastElementChild.style.display = 'block' : event.target.lastElementChild.style.display = ''   
+    event.target.lastElementChild.style.display === '' ? event.target.lastElementChild.style.display = 'flex' : event.target.lastElementChild.style.display = ''   
 }
 const addEvent = function (elem) {
     elem.addEventListener("click",openOrCloseDetails) 
